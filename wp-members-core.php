a9777aac5efbcbcb18dee2c6eb2374025e82ec29
cbutlerjr <cbutlerjr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-06 01:26:26 +0000

    patch for change password validation
    git-svn-id: https://plugins.svn.wordpress.org/wp-members/tags/2.5.2@393666 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-members-core.php
+++ wp-members-core.php
@@ -415,7 +415,7 @@ function wpmem_change_password()
 		$pass1 = $_POST['pass1'];
 		$pass2 = $_POST['pass2'];
 
-		if ($pass1 != $pass2) {
+		if ( ($pass1 != $pass2) || (!$pass1 && !$pass2) ) {
 
 			$wpmem_regchk = "pwdchangerr";
 
