2f59bd5ffc38290eab5f399dcc0796b034683044
Chad Butler <chad@butlerblog.com>
2015-02-26 10:43:23 -0600

    Update wp-members.php
    Implemented fixes for determining the stylesheet path

--- wp-members.php
+++ wp-members.php
@@ -3,7 +3,7 @@
 Plugin Name: WP-Members
 Plugin URI:  http://rocketgeek.com
 Description: WP access restriction and user registration.  For more information on plugin features, refer to <a href="http://rocketgeek.com/plugins/wp-members/users-guide/">the online Users Guide</a>. A <a href="http://rocketgeek.com/plugins/wp-members/quick-start-guide/">Quick Start Guide</a> is also available. WP-Members(tm) is a trademark of butlerblog.com.
-Version:     2.9.9 alpha
+Version:     2.9.8.1
 Author:      Chad Butler
 Author URI:  http://butlerblog.com/
 License:     GPLv2
@@ -60,7 +60,7 @@ License:     GPLv2
 
 
 /** initial constants **/
-define( 'WPMEM_VERSION', '2.9.9' );
+define( 'WPMEM_VERSION', '2.9.8.1' );
 define( 'WPMEM_DEBUG', false );
 define( 'WPMEM_DIR',  plugin_dir_url ( __FILE__ ) );
 define( 'WPMEM_PATH', plugin_dir_path( __FILE__ ) );
@@ -131,8 +131,8 @@ function wpmem_init()
 	/**
 	 * define the stylesheet
 	 */
-	$wpmem_style = get_option( 'wpmembers_cssurl', null );
-	$wpmem_style = ( ! $wpmem_style ) ? get_option( 'wpmembers_style', null ) : $wpmem_style;
+	$wpmem_style = get_option( 'wpmembers_style', null );
+	$wpmem_style = ( $wpmem_style == 'use_custom' || ! $wpmem_style ) ? get_option( 'wpmembers_cssurl', null ) : $wpmem_style;
 	define( 'WPMEM_CSSURL', $wpmem_style );
 
 
