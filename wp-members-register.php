b8117d380aa869708feec9abafa1e14172f054ad
Chad Butler <chad@butlerblog.com>
2015-04-20 10:03:19 -0500

    fix from previous commit

--- wp-members-register.php
+++ wp-members-register.php
@@ -140,7 +140,7 @@ function wpmem_registration( $toggle ) {
 					$privatekey,
 					$_SERVER["REMOTE_ADDR"],
 					$_POST["recaptcha_challenge_field"],
-					$_POST["recaptcha_response_field"],
+					$_POST["recaptcha_response_field"]
 				);
 
 				if( ! $resp->is_valid ) {
