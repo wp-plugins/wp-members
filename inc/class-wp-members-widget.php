be9ccde6efe6617cc716c95805cefb4aa7acfbf6
Chad Butler <chad@butlerblog.com>
2015-07-15 11:51:26 -0500

    tested and debugged widget __construct for WP 4.3

--- inc/class-wp-members-widget.php
+++ inc/class-wp-members-widget.php
@@ -10,11 +10,14 @@ class widget_wpmemwidget extends WP_Widget {
 	 * Sets up the WP-Members login widget.
 	 */
 	function __construct() {
-		$widget_ops = array( 
-			'classname'   => 'wp-members', 
-			'description' => __( 'Displays the WP-Members sidebar login.', 'wp-members' ) 
-			); 
-		$this->WP_Widget( 'widget_wpmemwidget', 'WP-Members Login', $widget_ops );
+		parent::__construct(
+			'widget_wpmemwidget',
+			'WP-Members Login',
+			array( 
+				'classname'   => 'wp-members', 
+				'description' => __( 'Displays the WP-Members sidebar login.', 'wp-members' ), 
+			)
+		);
 	}
 
 	/**
