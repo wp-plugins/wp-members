a4115c0832b269f4241a4caf558bc0f86684ecc8
Chad Butler <chad@butlerblog.com>
2015-09-28 09:06:17 -0500

    fix to load tinymce shortcode button on new post/page (previously was only loading on edit).

--- inc/class-wp-members-tinymce-buttons.php
+++ inc/class-wp-members-tinymce-buttons.php
@@ -6,20 +6,31 @@
  */
 class WP_Members_TinyMCE_Buttons {
 	
+	/**
+	 * Initialize WP-Members TinyMCE Button.
+	 *
+	 * @since 3.0
+	 */
 	function __construct() {
-    	add_action( 'admin_head', array( &$this, 'init' ) );
-    }
-	
-    function init() {
-		if ( ! current_user_can( 'edit_posts' ) && ! current_user_can( 'edit_pages' ) )
-			return;		
+    	
+		if ( ! current_user_can( 'edit_posts' ) && ! current_user_can( 'edit_pages' ) ) {
+			return;
+		}
 
-		if ( get_user_option('rich_editing') == 'true' ) {  
+		if ( get_user_option( 'rich_editing' ) == 'true' ) {  
 			add_filter( 'mce_external_plugins', array( &$this, 'add_plugin' ) );  
 			add_filter( 'mce_buttons', array( &$this, 'register_button' ) ); 
 		}  
     }  
 
+	/**
+	 * Load the correct plugin file.
+	 *
+	 * @since 3.0
+	 *
+	 * @param  array $plugin_array
+	 * @return array $plugin_array
+	 */
 	function add_plugin( $plugin_array ) {  
 		
 		$ver = WPMEM_VERSION;
@@ -32,7 +43,15 @@ class WP_Members_TinyMCE_Buttons {
 		}
 		return $plugin_array; 
 	}
-	
+
+	/**
+	 * Register the button.
+	 *
+	 * @since 3.0
+	 *
+	 * @param  array $buttons
+	 * @return array $buttons
+	 */	
 	function register_button( $buttons ) {  
 		array_push( $buttons, "wpmem_shortcodes_button" );
 		return $buttons; 
