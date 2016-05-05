c0a96aef0ffdaf9e9222b0e5a02e262b88b98099
Chad Butler <chad@butlerblog.com>
2016-05-05 14:05:40 -0500

    updated admin side warning messages, code cleanup

--- admin/dialogs.php
+++ admin/dialogs.php
@@ -23,7 +23,7 @@
  */
 
  
-/** Actions */
+// Actions.
 if ( ! is_multisite() ) {
 	add_action('wp_dashboard_setup', 'butlerblog_dashboard_widget');
 }
@@ -32,9 +32,9 @@ if ( ! is_multisite() ) {
 /**
  * Outputs the various admin warning messages.
  *
- * @since 2.8
+ * @since 2.8.0
  * 
- * @param string $did_update Contains the update message.
+ * @param string $did_update     Contains the update message.
  * @param array  $wpmem_settings Array containing the plugin settings.
  */
 function wpmem_a_do_warnings( $did_update ) {
@@ -52,36 +52,49 @@ function wpmem_a_do_warnings( $did_update ) {
  	 */
 
 	// Are warnings turned off?
-	$warnings_off = ( $wpmem->warnings == 0 ) ? true : false;
+	$warnings_on = ( $wpmem->warnings == 0 ) ? true : false;
+	
+	// Is there an active warning?
+	$warning_active = false;
 
 	// Settings allow anyone to register.
-	if ( get_option( 'users_can_register' ) != 0 && $warnings_off ) {
-		wpmem_a_warning_msg(1);
+	if ( get_option( 'users_can_register' ) != 0 && $warnings_on ) {
+		wpmem_a_warning_msg( 'users_can_register' );
+		$warning_active = true;
 	}
 
 	// Settings allow anyone to comment.
-	if ( get_option( 'comment_registration' ) !=1 && $warnings_off ) {
-		wpmem_a_warning_msg(2);
+	if ( get_option( 'comment_registration' ) !=1 && $warnings_on ) {
+		wpmem_a_warning_msg( 'comment_registration' );
+		$warning_active = true;
 	}
 
 	// Rss set to full text feeds.
-	if ( get_option( 'rss_use_excerpt' ) !=1 && $warnings_off ) {
-		wpmem_a_warning_msg(3);
+	if ( get_option( 'rss_use_excerpt' ) !=1 && $warnings_on ) {
+		wpmem_a_warning_msg( 'rss_use_excerpt' );
+		$warning_active = true;
 	}
 
 	// Holding registrations but haven't changed default successful registration message.
-	if ( $warnings_off && $wpmem->mod_reg == 1 && $wpmem_dialogs[3] == 'Congratulations! Your registration was successful.<br /><br />You may now login using the password that was emailed to you.' ) {
-		wpmem_a_warning_msg(4);
+	if ( $warnings_on && $wpmem->mod_reg == 1 && $wpmem_dialogs['success'] == $wpmem->get_text( 'success' ) ) {
+		wpmem_a_warning_msg( 'success' );
+		$warning_active = true;
 	}
 
 	// Haven't entered recaptcha api keys.
-	if ( $warnings_off && $wpmem->captcha == 1 ) {
-		$wpmem_captcha = get_option( 'wpmembers_captcha' );
+	if ( $warnings_on && $wpmem->captcha == 1 ) {
+		$wpmem_captcha = get_option( 'wpmembers_captcha' ); echo '<pre>'; print_r( $wpmem_captcha ); echo '</pre>';
 		if ( isset( $wpmem_captcha['recaptcha'] ) ) {
 			if ( ! $wpmem_captcha['recaptcha']['public'] || ! $wpmem_captcha['recaptcha']['private'] ) {
-				wpmem_a_warning_msg(6);
+				wpmem_a_warning_msg( 'wpmembers_captcha' );
+				$warning_active = true;
 			}
 		}
+	}
+	
+	// If there is an active warning, display message about warnings.
+	if ( $warning_active ) {
+		wpmem_a_warning_msg( 'warning_active' );
 	}
 
 }
@@ -91,8 +104,9 @@ function wpmem_a_do_warnings( $did_update ) {
  * Assembles the various admin warning messages.
  *
  * @since 2.4.0
+ * @since 3.1.0 Changed $msg argument to string.
  * 
- * @param int $msg The number for which message should be displayed.
+ * @param string $msg The number for which message should be displayed.
  */
 function wpmem_a_warning_msg( $msg ) {
 
@@ -100,37 +114,37 @@ function wpmem_a_warning_msg( $msg ) {
 
 	switch ( $msg ) {
 
-	case 1:
+	case 'users_can_register':
 		$strong_msg = __( 'Your WP settings allow anyone to register - this is not the recommended setting.', 'wp-members' );
 		$remain_msg = sprintf( __( 'You can %s change this here %s making sure the box next to "Anyone can register" is unchecked.', 'wp-members'), '<a href="options-general.php">', '</a>' );
 		$span_msg   = __( 'This setting allows a link on the /wp-login.php page to register using the WP native registration process thus circumventing any registration you are using with WP-Members. In some cases, this may suit the users wants/needs, but most users should uncheck this option. If you do not change this setting, you can choose to ignore these warning messages under WP-Members Settings.', 'wp-members' );
 		break;
 
-	case 2:
+	case 'comment_registration':
 		$strong_msg = __( 'Your WP settings allow anyone to comment - this is not the recommended setting.', 'wp-members' );
 		$remain_msg = sprintf( __( 'You can %s change this here %s by checking the box next to "Users must be registered and logged in to comment."', 'wp-members' ), '<a href="options-discussion.php">', '</a>' );
 		$span_msg   = __( 'This setting allows any users to comment, whether or not they are registered. Depending on how you are using WP-Members will determine whether you should change this setting or not. If you do not change this setting, you can choose to ignore these warning messages under WP-Members Settings.', 'wp-members' );
 		break;
 
-	case 3:
+	case 'rss_use_excerpt':
 		$strong_msg = __( 'Your WP settings allow full text rss feeds - this is not the recommended setting.', 'wp-members' );
 		$remain_msg = sprintf( __( 'You can %s change this here %s by changing "For each article in a feed, show" to "Summary."', 'wp-members' ), '<a href="options-reading.php">' , '</a>' );
 		$span_msg   = __( 'Leaving this set to full text allows anyone to read your protected content in an RSS reader. Changing this to Summary prevents this as your feeds will only show summary text.', 'wp-members' );
 		break;
 
-	case 4:
+	case 'success':
 		$strong_msg = __( 'You have set WP-Members to hold registrations for approval', 'wp-members' );
 		$remain_msg = __( 'but you have not changed the default message for "Registration Completed" under "WP-Members Dialogs and Error Messages."  You should change this message to let users know they are pending approval.', 'wp-members' );
 		break;
 
-	case 5:
-		$strong_msg = __( 'You have set WP-Members to turn off the registration process', 'wp-members' );
-		$remain_msg = __( 'but you also set to moderate and/or email admin new registrations.  You will need to set up a registration page for users to register.', 'wp-members' );
-		break;
-
-	case 6:
+	case 'wpmembers_captcha':
 		$strong_msg = __( 'You have turned on reCAPTCHA', 'wp-members');
 		$remain_msg = __( 'but you have not entered API keys.  You will need both a public and private key.  The CAPTCHA will not display unless a valid API key is included.', 'wp-members' );
+		break;
+		
+	case 'warning_active':
+		$strong_msg = __( 'You have active settings that are not recommended.', 'wp-members' );
+		$remain_msg = __( 'If you will not be changing these settings, you can turn off these warning messages by checking the "Ignore warning messages" in the settings below.', 'wp-members' );
 		break;
 
 	}
@@ -146,7 +160,7 @@ function wpmem_a_warning_msg( $msg ) {
 /**
  * Assemble the side meta box.
  *
- * @since 2.8
+ * @since 2.8.0
  */
 function wpmem_a_meta_box() {
 
