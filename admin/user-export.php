c3036a31225786eaf31cba2ec5bd4819bd1557b5
Chad Butler <chad@butlerblog.com>
2017-01-04 09:39:43 -0600

    added filter option in user export to decode html entities in user fields

--- admin/user-export.php
+++ admin/user-export.php
@@ -35,6 +35,7 @@ function wpmem_export_users( $args, $users = null ) {
 		'filename'       => 'wp-members-user-export-' . $today . '.csv',
 		'export_fields'  => wpmem_fields(), //array(),
 		'exclude_fields' => array( 'password', 'confirm_password', 'confirm_email' ),
+		'entity_decode'  => false,
 	);
 
 	// Merge $args with defaults.
@@ -97,10 +98,12 @@ function wpmem_export_users( $args, $users = null ) {
 		foreach ( $args['export_fields'] as $meta => $field ) {
 			if ( ! in_array( $meta, $args['exclude_fields'] ) ) {
 				// @todo Research using fputcsv to escape fields for export.
-				if ( in_array( $meta, $wp_user_fields ) ){
+				if ( in_array( $meta, $wp_user_fields ) ) {
 					$data .= '"' . $user_info->{$meta} . '",';	
 				} else {
-					$data .= '"' . get_user_meta( $user, $meta, true ) . '",';
+					$raw_data  = get_user_meta( $user, $meta, true );
+					$formatted = ( $args['entity_decode'] ) ? html_entity_decode( $raw_data ) : $raw_data;
+					$data .= '"' . $formatted . '",';
 				}
 			}
 		}
