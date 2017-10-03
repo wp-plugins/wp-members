a35b0c28c26b9209c23447c59e7965170f93a122
Chad Butler <chad@butlerblog.com>
2017-10-03 12:58:46 -0500

    added wpmem_get_sub_str() utility

--- inc/utilities.php
+++ inc/utilities.php
@@ -331,4 +331,51 @@ function wpmem_convert_tag( $tag ) {
 	return $tag;
 }
 
+/**
+ * String manipulation utility.
+ *
+ * Manipulates a given string based on the location of another string to return
+ * a requested part or parts of the original string.  For extracting a string
+ * to get what is before or after, the returned result is a string.  If the
+ * string is requested to be "split" by the needle string, an array containing
+ * the parts before, after, and the "needle" are returned.
+ *
+ * @since 3.2.0
+ *
+ * @param  string       $needle
+ * @param  string       $haystack
+ * @param  string       $position (before|after|split default: 'after')
+ * @param  boolean      $keep_needle (default:true)
+ * @return string|array $new {
+ *     An array of the original string, as split by the "needle" string.
+ *
+ *     @type string $before
+ *     @type string $after
+ *     @type string $needle
+ * }
+ */
+function wpmem_get_sub_str( $needle, $haystack, $position = 'after', $keep_needle = true ) {
+	$pos = strpos( $haystack, $needle );
+	if ( false === $pos ) {
+		return $haystack;
+	} else {
+		if ( 'before' == $position ) {
+			$new = ( substr( $haystack, 0, $pos ) );
+			$new = ( $keep_needle ) ? $string . $needle : $new;
+		} elseif ( 'after' == $position ) {
+			$new = ( substr( $haystack, $pos+strlen( $needle ) ) );
+			$new = ( $keep_needle ) ? $needle . $string : $new;
+		} elseif ( 'split' == $position ) {
+			$before = ( substr( $haystack, 0, $pos ) );
+			$after  = ( substr( $haystack, $pos+strlen( $needle ) ) );
+			$new    = array(
+				'before' => $before,
+				'after'  => $after,
+				'needle' => $needle,
+			);
+		}
+	}
+	return $new;
+}
+
 // End of file.
