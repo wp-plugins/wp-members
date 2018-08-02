ba667558077e8dfb36d27e47b21c0d5bd0be46ce
Chad Butler <chad@butlerblog.com>
2018-08-02 09:44:03 -0400

    moved wpmem_wp_reserved_terms() to admin api

--- inc/utilities.php
+++ inc/utilities.php
@@ -199,30 +199,6 @@ endif;
 
 
 /**
- * Returns an array of WordPress reserved terms.
- *
- * @since 3.0.2
- *
- * @return array An array of WordPress reserved terms.
- */
-function wpmem_wp_reserved_terms() {
-	$reserved_terms = array( 'attachment', 'attachment_id', 'author', 'author_name', 'calendar', 'cat', 'category', 'category__and', 'category__in', 'category__not_in', 'category_name', 'comments_per_page', 'comments_popup', 'customize_messenger_channel', 'customized', 'cpage', 'day', 'debug', 'error', 'exact', 'feed', 'fields', 'hour', 'link_category', 'm', 'minute', 'monthnum', 'more', 'name', 'nav_menu', 'nonce', 'nopaging', 'offset', 'order', 'orderby', 'p', 'page', 'page_id', 'paged', 'pagename', 'pb', 'perm', 'post', 'post__in', 'post__not_in', 'post_format', 'post_mime_type', 'post_status', 'post_tag', 'post_type', 'posts', 'posts_per_archive_page', 'posts_per_page', 'preview', 'robots', 'role', 's', 'search', 'second', 'sentence', 'showposts', 'static', 'subpost', 'subpost_id', 'tag', 'tag__and', 'tag__in', 'tag__not_in', 'tag_id', 'tag_slug__and', 'tag_slug__in', 'taxonomy', 'tb', 'term', 'theme', 'type', 'w', 'withcomments', 'withoutcomments', 'year' );
-	
-	/**
-	 * Filter the array of reserved terms.
-	 *
-	 * @since 3.0.2
-	 *
-	 * @param array $reserved_terms
-	 */
-	$reserved_terms = apply_filters( 'wpmem_wp_reserved_terms', $reserved_terms );
-	
-	return $reserved_terms;
-}
-
-
-
-/**
  * Convert form tag.
  *
  * @todo This is temporary to handle form tag conversion.
