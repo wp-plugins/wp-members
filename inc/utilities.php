7af08a05c81baff141c6d70285739b8e6b7e5034
Chad Butler <chad@butlerblog.com>
2018-08-02 09:31:56 -0400

    move wpmem_use_ssl(), wpmem_write_log(), and wpmem_get_sub_str() to utilities api

--- inc/utilities.php
+++ inc/utilities.php
@@ -20,9 +20,7 @@
  * - wpmem_texturize
  * - wpmem_enqueue_style
  * - wpmem_do_excerpt
- * - wpmem_use_ssl
  * - wpmem_wp_reserved_terms
- * - wpmem_write_log
  */
 
 // Exit if accessed directly.
@@ -198,16 +196,6 @@ endif;
 
 
 
-/**
- * Returns http:// or https:// depending on ssl.
- *
- * @since 2.9.8
- *
- * @return string https://|http:// depending on whether ssl is being used.
- */
-function wpmem_use_ssl() {
-	return ( is_ssl() ) ? 'https://' : 'http://';
-}
 
 
 /**
@@ -233,20 +221,6 @@ function wpmem_wp_reserved_terms() {
 }
 
 
-/**
- * Log debugging errors.
- *
- * @since 3.1.2
- * 
- * @param mixed (string|array|object) $log Information to write in the WP debug file.
- */
-function wpmem_write_log ( $log ) {
-	if ( is_array( $log ) || is_object( $log ) ) {
-		error_log( print_r( $log, true ) );
-	} else {
-		error_log( $log );
-	}
-}
 
 /**
  * Convert form tag.
@@ -287,51 +261,5 @@ function wpmem_convert_tag( $tag ) {
 	return $tag;
 }
 
-/**
- * String manipulation utility.
- *
- * Manipulates a given string based on the location of another string to return
- * a requested part or parts of the original string.  For extracting a string
- * to get what is before or after, the returned result is a string.  If the
- * string is requested to be "split" by the needle string, an array containing
- * the parts before, after, and the "needle" are returned.
- *
- * @since 3.2.0
- *
- * @param  string       $needle
- * @param  string       $haystack
- * @param  string       $position (before|after|split default: 'after')
- * @param  boolean      $keep_needle (default:true)
- * @return string|array $new {
- *     An array of the original string, as split by the "needle" string.
- *
- *     @type string $before
- *     @type string $after
- *     @type string $needle
- * }
- */
-function wpmem_get_sub_str( $needle, $haystack, $position = 'after', $keep_needle = true ) {
-	$pos = strpos( $haystack, $needle );
-	if ( false === $pos ) {
-		return $haystack;
-	} else {
-		if ( 'before' == $position ) {
-			$new = ( substr( $haystack, 0, $pos ) );
-			$new = ( $keep_needle ) ? $string . $needle : $new;
-		} elseif ( 'after' == $position ) {
-			$new = ( substr( $haystack, $pos+strlen( $needle ) ) );
-			$new = ( $keep_needle ) ? $needle . $string : $new;
-		} elseif ( 'split' == $position ) {
-			$before = ( substr( $haystack, 0, $pos ) );
-			$after  = ( substr( $haystack, $pos+strlen( $needle ) ) );
-			$new    = array(
-				'before' => $before,
-				'after'  => $after,
-				'needle' => $needle,
-			);
-		}
-	}
-	return $new;
-}
 
 // End of file.
