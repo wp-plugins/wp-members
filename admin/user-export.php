e3f0624c4afb7f364afeb2b1ea321ea718b34af8
Chad Butler <chad@butlerblog.com>
2018-11-28 07:18:09 -0500

    revised export function, added wpmem_export_fields filter

--- admin/user-export.php
+++ admin/user-export.php
@@ -52,6 +52,33 @@ function wpmem_export_users( $args, $users = null ) {
 	 * @param array $args An array of arguments to merge with defaults. Default null.
 	 */
 	$args = wp_parse_args( apply_filters( 'wpmem_export_args', $args ), $defaults );
+	
+	// Prepare fields, add additional "special" fields.
+	$export_fields = array(
+		'ID'       => __( 'User ID', 'wp-members' ),
+		'username' => __( 'Username', 'wp-members' ),
+	);
+	foreach( $args['export_fields'] as $meta_key => $value ) {
+		$export_fields[ $meta_key ] = $value['label'];
+	}
+	if ( 1 == $wpmem->mod_reg ) {
+		$export_fields['active'] = __( 'Activated?', 'wp-members');
+	}
+	if ( defined( 'WPMEM_EXP_MODULE' ) && 1 == $wpmem->use_exp ) {
+		$export_fields['exp_type'] = __( 'Subscription', 'wp-members' );
+		$export_fields['expires']  = __( 'Expires', 'wp-members' );
+	}
+	$export_fields['user_registered'] = __( 'Registered', 'wp-members' );
+	$export_fields['wpmem_reg_ip']    = __( 'IP', 'wp-members' );
+	
+	/**
+	 * Filter the export fields.
+	 *
+	 * @since 3.2.5
+	 *
+	 * @param array $export_fields
+	 */
+	$export_fields = apply_filters( 'wpmem_export_fields', $export_fields );
 
 	// Output needs to be buffered, start the buffer.
 	ob_start();
@@ -68,27 +95,15 @@ function wpmem_export_users( $args, $users = null ) {
 	$handle = fopen( 'php://output', 'w' );
 	fputs( $handle, "\xEF\xBB\xBF" ); // UTF-8 BOM
 
-	$header = array( 'ID' => 'User ID', 'username' => 'Username' );
 	// Remove excluded fields from $export_fields while setting up $header array.
-	foreach ( $args['export_fields'] as $meta => $field ) {
+	$header = array();
+	foreach ( $export_fields as $meta => $field ) {
 		if ( in_array( $meta, $args['exclude_fields'] ) ) {
-			unset( $args['export_fields'][ $meta ] );
+			unset( $export_fields[ $meta ] );
 		} else {
-			$header[ $meta ] = $field['label'];
+			$header[ $meta ] = $field;
 		}
 	}
-
-	if ( 1 == $wpmem->mod_reg ) {
-		$header['active'] = __( 'Activated?', 'wp-members');
-	}
-
-	if ( defined( 'WPMEM_EXP_MODULE' ) && 1 == $wpmem->use_exp ) {
-		$header['exp_type'] = __( 'Subscription', 'wp-members' );
-		$header['expires']  = __( 'Expires', 'wp-members' );
-	}
-
-	$header['user_registered'] = __( 'Registered', 'wp-members' );
-	$header['wpmem_reg_ip']    = __( 'IP', 'wp-members' );
 	
 	/**
 	 * Filters user export header row before assembly.
@@ -102,33 +117,45 @@ function wpmem_export_users( $args, $users = null ) {
 	fputcsv( $handle, $header );
 
 	// Loop through the array of users, assemble csv.
-	// $args['export_fields'] only includes fields to be exported at this point.
+	// $export_fields only includes fields to be exported at this point.
 	foreach ( $users as $user ) {
 
 		$user_info = get_userdata( $user );
 
 		$wp_user_fields = [ 'username', 'user_email', 'user_nicename', 'user_url', 'display_name' ];
-		$row = array( 'ID' => $user_info->ID, 'username' => $user_info->user_login );
-		foreach ( $args['export_fields'] as $meta => $field ) {
-			if ( in_array( $meta, $wp_user_fields ) ) {
-				$row[ $meta ] = ( 'username' == $meta ) ? $user_info->user_login : $user_info->{$meta};
-			} else {
-				$raw_data = get_user_meta( $user, $meta, true );
-				$row[ $meta ]    = ( $args['entity_decode'] ) ? html_entity_decode( $raw_data ) : $raw_data;
+		foreach ( $export_fields as $meta => $field ) {
+			switch ( $meta ) {
+				case 'ID':
+					$row[ $meta ] = $user_info->ID;
+					break;
+				case 'username':
+					$row[ $meta ] = $user_info->user_login;
+					break;
+				case 'active':
+					$row[ $meta ] = get_user_meta( $user, 'active', 1 ) ? __( 'Yes' ) : __( 'No' );
+					break;
+				case 'exp_type':
+					$row['exp_type'] = get_user_meta( $user, 'exp_type', true );
+					break;
+				case 'expires':
+					$row['expires'] = get_user_meta( $user, 'expires', true );
+					break;
+				case 'user_registered':
+					$row['user_registered'] = $user_info->user_registered;
+					break;
+				case 'wpmem_reg_ip':
+					$row['wpmem_reg_ip'] = get_user_meta( $user, 'wpmem_reg_ip', true );
+					break;
+				default:
+					if ( in_array( $meta, $wp_user_fields ) ) {
+						$row[ $meta ] = ( 'username' == $meta ) ? $user_info->user_login : $user_info->{$meta};
+					} else {
+						$raw_data = get_user_meta( $user, $meta, true );
+						$row[ $meta ] = ( $args['entity_decode'] ) ? html_entity_decode( $raw_data ) : $raw_data;
+					}
+					break;
 			}
 		}
-
-		if ( 1 == $wpmem->mod_reg ) {
-			$row['active'] = get_user_meta( $user, 'active', 1 ) ? __( 'Yes' ) : __( 'No' );
-		}
-
-		if ( defined( 'WPMEM_EXP_MODULE' ) && 1 == $wpmem->use_exp ) {
-			$row['exp_type'] = get_user_meta( $user, 'exp_type', true );
-			$row['expires']  = get_user_meta( $user, 'expires', true );
-		}
-
-		$row['user_registered'] = $user_info->user_registered;
-		$row['wpmem_reg_ip']    = get_user_meta( $user, 'wpmem_reg_ip', true );
 
 		/**
 		 * Filter the user data before assembly.
