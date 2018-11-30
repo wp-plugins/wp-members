678495028970e350dd481adc8858743a363d09f7
Chad Butler <chad@butlerblog.com>
2018-11-30 13:09:31 -0500

    recaptcha v1 totally obsolete

--- inc/forms.php
+++ inc/forms.php
@@ -195,19 +195,7 @@ function wpmem_inc_recaptcha( $arr ) {
 	$http = wpmem_use_ssl();
 
 	global $wpmem;
-	if ( $wpmem->captcha == 1 ) {
-
-		$str  = '<script type="text/javascript">
-			var RecaptchaOptions = { theme : \''. $arr['theme'] . '\'' . $lang . ' };
-		</script>
-		<script type="text/javascript" src="' . $http . 'www.google.com/recaptcha/api/challenge?k=' . $arr['public'] . '"></script>
-		<noscript>
-			<iframe src="' . $http . 'www.google.com/recaptcha/api/noscript?k=' . $arr['public'] . '" height="300" width="500" frameborder="0"></iframe><br/>
-			<textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>
-			<input type="hidden" name="recaptcha_response_field" value="manual_challenge"/>
-		</noscript>';
-	
-	} elseif ( $wpmem->captcha == 3 ) {
+	if ( $wpmem->captcha == 3 ) {
 		
 		$str = '<script src="https://www.google.com/recaptcha/api.js" async defer></script>
 		<div class="g-recaptcha" data-sitekey="' . $arr['public'] . '"></div>';
