b4c0a6ee5e3b569769942566fd6ff5e39c4afc73
RocketGeek <support@rocketgeek.com>
2019-10-26 20:07:02 -0400

    added tab-options.php for legacy purposes

---
+++ admin/tab-options.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Included for legacy reasons: backward compatibility with
+ * WP-Members User List 1.9.4 and earlier. Those will actually
+ * still function, and there is no front end issue, but removing
+ * this file causes a PHP notice in the admin panel.
+ *
+ * Remove later.
+ */
