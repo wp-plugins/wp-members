f8653c5a155633e53bd31dbce0b91d45c0658517
RocketGeek <support@rocketgeek.com>
2020-01-13 13:21:37 -0500

    added legacy dialogs file for backwards compatibility purposes

---
+++ inc/dialogs.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Included for legacy reasons: backward compatibility with
+ * WP-Members Advanced Options. Those will actually still
+ * function, and there is no front end issue, but removing
+ * this file causes a PHP notice for certain configurations.
+ *
+ * Remove later.
+ */
