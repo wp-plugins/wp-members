3108b325ef9aad34dff77d717783e0a25d725485
RocketGeek <support@rocketgeek.com>
2021-12-10 08:36:49 -0500

    debug if screen loads with no $post (i.e. 404)

--- includes/class-wp-members-validation-link.php
+++ includes/class-wp-members-validation-link.php
@@ -280,8 +280,7 @@ class WP_Members_Validation_Link {
 		} 
 
 		// Validation flag must be confirmed.
-		$validated = get_user_meta( $user->ID, $this->validation_confirm, true ); // @todo Update to use wpmem_is_user_confirmed().
-		if ( false == $validated ) {
+		if ( false == wpmem_is_user_confirmed( $user->ID ) ) {
 			return new WP_Error( 'authentication_failed', __( '<strong>ERROR</strong>: User has not confirmed their account.', 'wp-members' ) );
 		}
 
